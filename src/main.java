4103594e0ad81c7bc38980d03896f93e9ed6f1b3
cheflykas <54162521+cheflykas@users.noreply.github.com>
2019-10-30 00:02:09 -0700

    Added apcs(techlab) extension archive

--- src/main.java
+++ src/main.java
@@ -1,11 +1,13 @@
 import java.io.*;
 import java.util.*;
 import javax.swing.JFrame;	
+import apcs.Window;
 
 public class main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		Window.size(812, 500);
 		Scanner sc = new Scanner(new File("Periodic Table.txt"));
 		String[] data = new String[10];
 		for(int r = 0; r < 118; r++) {
